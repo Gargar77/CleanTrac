c4b05898f4126cd5a000f5f3aa43e8db378c8149
gargar77 <gbautista2@knights.ucf.edu>
2020-09-09 16:37:19 -0400

    basic auth methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@
 #
 class User < ApplicationRecord
     attr_reader :password
+    after_initialize :ensure_session_token
 
     validates :email, :first_name, :last_name, :session_token, presence:true, uniqueness:true
     validates :password_digest, presence: { message: 'password must not be blank'}
@@ -25,7 +26,44 @@ class User < ApplicationRecord
     foreign_key: :company_id,
     primary_key: :id
 
-    
+    def self.find_by_credentials(email,password)
+        user = User.find_by(email: email)
+
+        return nil if user.nil?
+
+        user && user.is_password?(password) ? user : nil
+    end
+
+    def password=(password)
+        @password = password
+
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def generate_session_token
+        token = SecureRandom.urlsafe_base64(16)
+
+        while self.class.exists?(session_token: token)
+            token = SecureRandom.urlsafe_base64(16)
+        end
+
+        token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_session_token
+        self.save!
+
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
 
 
 end
