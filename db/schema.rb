7a5076c4d3669413b90b5aedbb73db9532638924
gargar77 <gbautista2@knights.ucf.edu>
2020-09-10 13:12:44 -0400

    added account name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_201922) do
+ActiveRecord::Schema.define(version: 2020_09_10_170622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_201922) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "primary_contact_phone", null: false
+    t.string "name", null: false
     t.index ["address"], name: "index_accounts_on_address"
     t.index ["primary_contact_name"], name: "index_accounts_on_primary_contact_name"
   end
