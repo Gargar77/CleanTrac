5199331e2397e5e542a1e0cd2aefd1df8cb54db9
gargar77 <gbautista2@knights.ucf.edu>
2020-09-11 15:49:15 -0400

    added validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,8 +16,43 @@
 #
 
 
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe User, type: :model do
+    subject(:cleaner) do
+        FactoryBot.build(:cleaner)
+    end
+
+
+    describe "validations" do
+        it { should validate_presence_of(:first_name)}
+        it { should validate_presence_of(:last_name)}
+        it { should validate_presence_of(:email)}
+        it { should validate_presence_of(:password_digest)}
+        it { should validate_length_of(:password).is_at_least(8)}
+        
+        describe "ensure uniqueness" do
+          it "ensures uniqueness of email" do
+            cleaner.email = "mail@gmail.com"
+            cleaner.session_token = "token"
+            cleaner.save!
+
+            cleaner2 = FactoryBot.build(:cleaner,email: "mail@gmail.com",session_token: "another token")
+            cleaner2.save
+            expect(cleaner2.errors.full_messages).to_not eq(nil)
+          end
+          it "ensures uniqeness of password_digest" do
+            cleaner.password_digest = "password_digest"
+            cleaner.session_token = "token"
+            cleaner.save!
     
+            cleaner2 = FactoryBot.build(:cleaner,session_token:"another_token")
+            cleaner2.password_digest = "password_digest"
+            cleaner2.save
+    
+            expect(cleaner2.errors.full_messages).to_not eq(nil)
+          end
+    
+        end
+      end
 end
