832acfb1368d4a4d305dda9d2736d4fe45cff4dd
gargar77 <gbautista2@knights.ucf.edu>
2020-09-11 13:04:35 -0400

    created model test skeleton

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  company_id      :integer          not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  role            :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  leader_id       :integer
+#
+
+
+require 'spec_helper'
+
+RSpec.describe User, type: :model do
+    
+end
