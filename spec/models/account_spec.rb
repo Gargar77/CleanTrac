eb23f0e871d2ab0f83e9e40b72f61b52d8740159
gargar77 <gbautista2@knights.ucf.edu>
2020-09-11 17:18:44 -0400

    created company simple validation tests

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -23,16 +23,23 @@
 
 require 'rails_helper'
 
-RSpec.describe Account, type: :model do
+RSpec.describe Company, type: :model do
     subject(:company) do
         FactoryBot.create(:company)
+    end
+
+    subject(:account) do
+        FactoryBot.create(:account,company_id: company.id)
     end
 
 
     describe "validations" do
         it { should validate_presence_of(:name) }
+        it { should validate_presence_of(:address) }
+        it { should validate_presence_of(:primary_contact_name) }
+        it { should validate_presence_of(:primary_contact_phone) }
+        #TODO: figure out how to test have_many through
         # it { should have_many(:cleaners) }
-        # it { should have_one(:company) }
-        
+        it { should have_one(:company) }
     end
 end
