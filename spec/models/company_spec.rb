d26d265f52e53353e760235aad894aaba1342405
gargar77 <gbautista2@knights.ucf.edu>
2020-09-12 20:10:28 -0400

    added association specs

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -18,5 +18,6 @@ RSpec.describe Company, type: :model do
     describe "validations" do
         it { should validate_presence_of(:name) }
         it { should have_many(:users) }
+        it { should have_many(:accounts) }
     end
 end
