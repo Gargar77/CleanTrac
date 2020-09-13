7a542f40b8f9fd88e3b0988a94965245b3f31b34
gargar77 <gbautista2@knights.ucf.edu>
2020-09-12 21:14:01 -0400

    created specs

---
+++ spec/models/file_link_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe FileLink, type: :model do
+    subject(:file_link) do
+        user = double("user",:id=>1) 
+        account = double("account",:id=>1) 
+        FileLink.create(uploader_id: user.id, account_id: account.id, link: "www.youtube.com")
+    end
+
+    it { should validate_presence_of(:uploader_id) }
+    it { should validate_presence_of(:account_id) }
+    it { should validate_presence_of(:link) }
+    it { should belong_to(:uploader) }
+    it { should belong_to(:account) }
+end
