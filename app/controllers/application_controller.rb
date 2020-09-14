17e4a27118239df89bb62d2a14958f873f1a0371
gargar77 <gbautista2@knights.ucf.edu>
2020-09-14 12:50:39 -0400

    added basic auth methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+    protect_from_forgery with: :exception
+    helper_method :current_user, :logged_in?
+
+    private
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+        @current_user = user
+    end
+    
 end
