44e9108f890140e987393b0610fc62be32745f26
gargar77 <gbautista2@knights.ucf.edu>
2020-09-14 12:34:59 -0400

    established non api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new,:create,:show]
+  root to: 'static_pages#root'
+
+  resources :users, only: [:new,:create]
   resource :session, only: [:new,:create,:destroy]
 
+  namespace :api, defaults: {format: :json} do
 
-  # match '*path', to: 'pages#index',via: :all
+  end
 end
