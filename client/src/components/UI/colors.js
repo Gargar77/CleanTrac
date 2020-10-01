28d25321b5b6ddc78cdccd68c0f01aab4b4f7b88
gargar77 <gbautista2@knights.ucf.edu>
2020-10-01 16:19:46 -0400

    changed color pallete

--- client/src/components/UI/colors.js
+++ client/src/components/UI/colors.js
@@ -1,9 +1,20 @@
+// export const COLORS = {
+//     brightGreen:'#52F25D',
+//     darkGreen:'#348C1C',
+//     pastelGreen: '#ADF28D',
+//     limeGreen: '#AAF23D',
+//     yellowGreen: '#C8F230'
+// }
+
+
+// Author: Andrew-Evans Gaerlan
 export const COLORS = {
-    brightGreen:'#52F25D',
-    darkGreen:'#348C1C',
-    pastelGreen: '#ADF28D',
-    limeGreen: '#AAF23D',
-    yellowGreen: '#C8F230'
+    pureBlue: '#0D52FF',
+    darkBlue: '#0C85E8',
+    lightBlue: '#00CFFF',
+    greenBlue: '#0CE8D6',
+    pastelGreen: '#0DFFA7'
 }
+
 
 
