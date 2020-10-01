7de27bdfa8d586945cf275a65ae7b1dd5a809cce
gargar77 <gbautista2@knights.ucf.edu>
2020-10-01 09:32:22 -0400

    root redirects depending on auth status

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect,withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -9,18 +9,36 @@ import Accounts from './containers/Accounts';
 
 
 class App extends Component {
-
+  
   render() {
+
+    let content = (
+      <Switch>
+        <Route path="/auth" exact component={Auth}/>
+        <Redirect to="/auth"/>
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      content = (
+        // auth required routes
+      <Route path="/accounts" exact component={Accounts}/>
+      );
+    }
+
     return  (
          <div className="App">
-          <Route path="/accounts" exact component={Accounts}/>
-          <Route path="/auth" exact component={Auth}/>
+          {content}
         </div>
      
     );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
 
-
-export default App;
+export default withRouter(connect(mapStateToProps)(App));
