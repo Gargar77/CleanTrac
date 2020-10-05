102abd1ec3728eaf477cb6f436456f89b5b8c6de
gargar77 <gbautista2@knights.ucf.edu>
2020-10-05 12:14:11 -0400

    user info dispatches handled by userReducer now

--- client/src/store/reducers/accountsReducer.js
+++ client/src/store/reducers/accountsReducer.js
@@ -1,31 +1,14 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    userId:null,
-    firstName:null,
-    lastName:null,
-    role:null,
-    email:null,
-    phone:null,
-    leaderId:null
+    
 }
 
-const loadUser = (state,action) => {
-    return {
-        userId:action.data.userId,
-    firstName:action.data.firstName,
-    lastName:action.data.lastName,
-    role:action.data.role,
-    email:action.data.email,
-    phone:action.data.phone,
-    leaderId:action.data.leaderId
-    }
-}
 
 
 const reducer = (state = initialState,action) => {
     switch (action.type) {
-        case actionTypes.LOAD_USER_DATA: return loadUser(state,action);
+        
         default: return state;
     }
 }
--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './auth';
 import AccountsReducer from './accountsReducer';
+import UserReducer from './userReducer';
 
 
 const rootReducer = combineReducers({
     auth:AuthReducer,
-    accounts:AccountsReducer
+    accounts:AccountsReducer,
+    user: UserReducer
 })
 
 export default rootReducer;
