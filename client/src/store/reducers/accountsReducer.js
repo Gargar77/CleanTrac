41fa131a8acb7f22c05f3127def23f4995bdee88
gargar77 <gbautista2@knights.ucf.edu>
2020-10-10 19:33:33 -0400

    created account fetching reducers

--- client/src/store/reducers/accountsReducer.js
+++ client/src/store/reducers/accountsReducer.js
@@ -1,14 +1,41 @@
+import { accountFail } from '../actions/accounts';
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-   
+   fetching:false,
+   errors:null
 }
 
+const accountStart = (state,action) => {
+    return {
+        ...state,
+        fetching:true
+    }
+}
 
+const accountSuccess = (state,action) => {
+    return {
+        ...state,
+        errors:null,
+        fetching:false,
+        entities:action.data
+    }
+}
+
+const accountFail = (state,action) => {
+
+    return {
+        ...state,
+        error:action.error,
+        fetching:false
+    };
+}
 
 const reducer = (state = initialState,action) => {
     switch (action.type) {
-        
+        case actionTypes.GET_ACCOUNTS_START: return accountStart(state,action);
+        case actionTypes.GET_ACCOUNTS_SUCCESS: return accountSuccess(state,action);
+        case actionTypes.GET_ACCOUNTS_FAIL: return accountFail(state,action);
         default: return state;
     }
 }
