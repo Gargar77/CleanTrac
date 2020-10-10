857276fee079304e7549f954133068f286796c61
gargar77 <gbautista2@knights.ucf.edu>
2020-10-10 19:26:08 -0400

    created accounts dispatchers

---
+++ client/src/store/actions/accounts.js
@@ -0,0 +1,41 @@
+import * as actionTypes from './actionTypes';
+
+export const accountStart = () => {
+    return {
+        type:actionTypes.GET_ACCOUNTS_START
+    }
+}
+
+export const accountSuccess = (accountsData) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        data:accountsData
+    }
+}
+
+export const accountFail = (error) => {
+    return {
+        type:actionTypes.AUTH_FAIL,
+        error: error.message
+    }
+}
+
+
+export const fetchAccountsData = (authToken,dispatch) => {
+
+    dispatch(accountStart);
+
+    fetch('http://localhost:3001/api/accounts',{
+        method:'GET',
+        headers: {
+            'Accept': 'application/json',
+            'content-type':'application/json',
+            'Authorization' : 'Bearer ' + authToken
+        }
+    })
+        .then(res => res.json())
+            .then(jsonRes => {
+                dispatch(accountSuccess(jsonRes))
+            })
+        .catch(err => dispatch(accountFail(err)))
+}
