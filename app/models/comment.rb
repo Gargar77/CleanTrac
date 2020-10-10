e5a5ff5e6068458dbd88e57d0c570c285830f880
gargar77 <gbautista2@knights.ucf.edu>
2020-10-10 16:22:44 -0400

    temporaly removed inverse asociations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -15,12 +15,10 @@ class Comment < ApplicationRecord
     belongs_to :author,
     class_name:'User',
     foreign_key: :author_id,
-    primary_key: :id,
-    inverse_of: :comments
+    primary_key: :id
 
     belongs_to :post,
     class_name: 'Post',
-    foreign_key: :author_id,
-    primary_key: :id,
-    inverse_of: :comments
+    foreign_key: :post_id,
+    primary_key: :id
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -15,8 +15,7 @@ class Post < ApplicationRecord
 
     belongs_to :author,
     class_name: 'User',
-    foreign_key: :author_id,
-    inverse_of: :posts
+    foreign_key: :author_id
 
     has_many :comments,
     class_name: "Comment",
